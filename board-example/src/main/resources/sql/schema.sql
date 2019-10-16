65c0038ac91e98e3ae73b0949768cba738f7ecaa
bestdeveloper0366 <bestdeveloper0366@gmail.com>
2019-10-16 22:53:13 +0900

    springboot-example

---
+++ board-example/src/main/resources/sql/schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS board;
+
+CREATE TABLE board (
+    id          BIGINT        PRIMARY KEY   AUTO_INCREMENT,
+    name        VARCHAR(255),
+    title       VARCHAR(255),
+    author       VARCHAR(255),
+    content     VARCHAR(255),
+    create_date TIMESTAMP,
+);
+
+DROP TABLE IF EXISTS comment;
+
+CREATE TABLE comment (
+    id          BIGINT        PRIMARY KEYNAME TITLE AUTHOR CONTENT    AUTO_INCREMENT,
+    content     VARCHAR(255),
+    create_date TIMESTAMP,
+    board_id    BIGINT NOT NULL,
+    CONSTRAINT FKLIJ9OOR1NAV89JEAT35S6KBP1 FOREIGN KEY (board_id) REFERENCES board
+);
